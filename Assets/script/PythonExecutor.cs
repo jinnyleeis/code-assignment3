e65c45d0ef4949f954ee5408bb7cb930019d8ce1
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-11-11 12:29:27 +0900

    .

--- Assets/script/PythonExecutor.cs
+++ Assets/script/PythonExecutor.cs
@@ -19,7 +19,7 @@ using UnityEngine;
 
 namespace PythonExecutor
 {
-    class Program:MonoBehaviour
+    class Program
     {    // 환경설정 Path를 설정하는 함수이다. 실제 Path가 바뀌는 건 아니고 프로그램 세션 안에서만 path를 변경해서 사용한다.
 
         //public static void DisplayNDarray(NDarray nDarray, string name = "")
